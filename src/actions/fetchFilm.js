e8a640c985b427a9ae0cd9fbeb7fdea634f53bbc
Kevin <1041874421@qq.com>
2017-06-19 20:33:11 +0800

    解决了出现的BUG的问题了，happy~ thunk并不是没有用只是很是没有完全理解中间件的概念， 还有写法有些多余的读物 #4

--- src/actions/fetchFilm.js
+++ src/actions/fetchFilm.js
@@ -5,24 +5,40 @@ const ROOT_URL = `https://api.douban.com`;
 
 export const FETCH_FILM = "FETCH_FILM";
 
-export function fetchFilm(){
+export function fetchFilm() {
     const url = `${ROOT_URL}/v2/movie/in_theaters`
     // console.log(url)
-    let request;
-    jsonp(url,null,function(err,data){
-        if(err){
-            console.error(err.message)
-        }else{
-            request =  data.subjects
-            store.dispatch({
-                type:FETCH_FILM,
-                payload:request
-            })
-        }
+    let request = new Promise((resolve, reject) => {
+        jsonp(url, null, function (err, data) {
+            if (err) {
+                console.error(err.message)
+            } else {
+               resolve(data.subjects) 
+            }
+        })
     })
+    // jsonp(url, null, function (err, data) {
+    //     if (err) {
+    //         console.error(err.message)
+    //     } else {
+    //         request = data.subjects
+    //         store.dispatch({
+    //             type:FETCH_FILM,
+    //             payload:request
+    //         })
+    //     }
+    // })
 
-    return {
-        type:FETCH_FILM,
-        payload:request
+    // return {
+    //     type:FETCH_FILM,
+    //     payload:request
+    // }
+    return (dispatch) => {
+       request.then(function(films){
+           return dispatch({
+               type: FETCH_FILM,
+               payload: films
+           })
+       })
     }
 }
