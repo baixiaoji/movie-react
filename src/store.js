780dd661e11167336f842b9c1c96ee70aed33d90
zouzou <1041874421@qq.com>
2017-08-15 14:17:24 +0800

    add the )

--- src/store.js
+++ src/store.js
@@ -19,6 +19,7 @@ const store = createStore(rootRudcer,
                                applyMiddleware(logger,thunk),
                                window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
                             )
+                         )
 
 export const history = syncHistoryWithStore(browserHistory, store)
 
